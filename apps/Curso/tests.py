730f63583f8949141a13f9642e11762b0a1f7f41
Karr110 <orquerakarlav@gmail.com>
2026-06-29 15:28:54 -0300

    Correccion de Templates

--- apps/Curso/tests.py
+++ apps/Curso/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Curso
+
+
+class CursoTemplateTests(TestCase):
+    def test_list_view_shows_course_name(self):
+        Curso.objects.create(anio="2024", division="A")
+
+        response = self.client.get(reverse("curso:curso_list"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "2024 A")
